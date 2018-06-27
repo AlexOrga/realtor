c2c6cc205f1a63cebbff1c8f15ea4ee83101ac47
Alex Orga <alexorga.dev@gmail.com>
2018-06-26 20:57:50 -0500

    retrieving listings data from firebase

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import connection from '../firebaseRequests/connection';
+import listingRequests from '../firebaseRequests/listings';
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
@@ -8,8 +9,19 @@ import ListingForm from '../components/ListingForm/ListingForm';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    listings: [],
+  }
+
   componentDidMount () {
     connection();
+    listingRequests.getRequest()
+      .then((listings) => {
+        this.setState({listings});
+      })
+      .catch((err) => {
+        console.error('error with listin GET', err);
+      });
   }
 
   render () {
