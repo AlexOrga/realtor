32ef2abe909411b6c195e0c3eb088a38b4b1c110
Alex Orga <alexorga.dev@gmail.com>
2018-07-03 19:02:04 -0500

    able to change state of address in listing form

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -3,10 +3,46 @@ import React from 'react';
 import './ListingForm.css';
 
 class ListingForm extends React.Component {
+  state = {
+    newListing: {
+      address: '',
+    },
+  }
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    console.error('you clicked submit', e);
+  }
+
+  addressChange = (e) => {
+    const tempListing = {...this.state.newListing};
+    tempListing.address = e.target.value;
+    this.setState({newListing: tempListing});
+  }
+
   render () {
+    const {newListing} = this.state;
+
     return (
-      <div className="ListingForm">
-        <h2>Listing Form</h2>
+      <div className="ListingForm col-xs-8 col-xs-offset-2">
+        <h2 className="text-center">Submit new property:</h2>
+        <form onSubmit={this.formSubmit}>
+          <div className="row">
+            <fieldset className="col-xs-3">
+              <label htmlFor="address">Address</label>
+              <br/>
+              <input
+                className="col-xs-12"
+                type="text"
+                id="address"
+                placeholder="1234 Main Street"
+                value={newListing.address}
+                onChange={this.addressChange}
+              />
+            </fieldset>
+          </div>
+          <button className="col-xs-6 btn btn-danger col-xs-offset-3">Save House</button>
+        </form>
       </div>
     );
   }
